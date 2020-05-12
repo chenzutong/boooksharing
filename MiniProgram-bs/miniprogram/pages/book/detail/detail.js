b5a0c67ba49c8832371a41ab9c1f2a0e6559e756
chenzutong <chenzutong912@163.com>
2020-05-13 00:21:49 +0800

    书籍添加时间

--- MiniProgram-bs/miniprogram/pages/book/detail/detail.js
+++ MiniProgram-bs/miniprogram/pages/book/detail/detail.js
@@ -32,6 +32,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log("getApp().globalData.bookDetail",getApp().globalData.bookDetail);
+    
     
     this.setData({
       data:getApp().globalData.bookDetail,
