b31832a9b450eb1567a2fa6e728d8d1aefbb43f3
chenzutong <chenzutong912@163.com>
2020-07-18 17:12:18 +0800

    手机正则表达式

--- MiniProgram-bs/miniprogram/pages/person/enroll/enroll.js
+++ MiniProgram-bs/miniprogram/pages/person/enroll/enroll.js
@@ -17,7 +17,7 @@ Page({
   }, // 注册
   regist: function (e) {
     var that = this
-    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})(17[0-9]{1}))+\d{8})$/;
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
     if (that.data.username == "") {
       wx.showModal({
         title: '提示',
